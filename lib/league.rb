e2c728409f12fd8abe38c81b79f2af95526da619
Reed Hillmar <reedhillmar@gmail.com>
2023-08-02 18:06:18 -0600

    test: add test code for League.add_team

--- lib/league.rb
+++ lib/league.rb
@@ -1,1 +1,12 @@
 #./lib/league.rb
+require "./lib/calculable"
+
+class League
+  include Calculable 
+
+  attr_reader :teams
+
+  def initialize
+    @teams = []
+  end
+end
--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -3,12 +3,23 @@ require "spec_helper"
 describe League do
   before(:each) do
     @league1 = League.new
+    @team1 = double("team1")
+    @team2 = double("team2")
   end
 
   describe "#initialize" do
     it "can exist and have details" do
       expect(@league1).to be_a League
       expect(@league1.teams).to eq([])
+    end
+  end
+
+  describe "#add_team" do
+    it "can add teams" do
+      @league1.add_team(@team1)
+      @league1.add_team(@team2)
+
+      expect(@league1.teams).to eq([@team1, @team2])
     end
   end
 end
