0a61cc5f43844f254e494727eebc3473e7a19e29
Nicholas McEnroe <nsmcenro@gmail.com>
2023-08-04 09:40:09 -0700

    Refactor: Add before each logic for season_stats_spec

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -6,66 +6,60 @@ require './lib/calculable'
 
 
 RSpec.describe Season do
+  before(:each) do
+    @searched_season = Season.new(20122013)
+  end
   describe '#initialize' do
     it "can initialize" do
-      season = Season.new(20122013)
-      expect(season).to be_a Season
+      expect(@searched_season).to be_a Season
     end
   end
 
   describe '#within_searched_season' do
     it "can return games within a season" do
-      searched_season2 = Season.new(20122013)
-      expect(searched_season2.within_searched_season).not_to eq nil
+      expect(@searched_season.within_searched_season).not_to eq nil
     end
   end
 
   describe '#most_tackles' do
     it "can return which team has the most tackles in a season" do
-      searched_season = Season.new(20122013)
-      expect(searched_season.most_tackles).to eq('Sporting Kansas City')
+      expect(@searched_season.most_tackles).to eq('Sporting Kansas City')
     end
   end
 
   describe '#least_tackles' do
     it "can return which team has the fewest tackles in a seasom" do
-      searched_season = Season.new(20122013)
-      expect(searched_season.fewest_tackles).to eq('FC Dallas')
+      expect(@searched_season.fewest_tackles).to eq('FC Dallas')
     end
   end
 
   describe '#least_accurate_team' do
     it "can return which team had the lowest shot to goal percentage in a season" do
-      searched_season = Season.new(20122013)
-      expect(searched_season.least_accurate_team).to eq('Washington Spirit FC')
+      expect(@searched_season.least_accurate_team).to eq('Washington Spirit FC')
     end
   end
 
   describe '#most_accurate_team' do
     it "can return which team had the highest shot to goal percentage in a season" do
-      searched_season = Season.new(20122013)
-      expect(searched_season.most_accurate_team).to eq('Atlanta United')
+      expect(@searched_season.most_accurate_team).to eq('Atlanta United')
     end
   end
 
   describe '#worst_coach' do
     it "can return the coach with the worst win loss percentage" do
-    searched_season = Season.new(20122013)
-    expect(searched_season.worst_coach).to eq('John Tortorella')
+    expect(@searched_season.worst_coach).to eq('John Tortorella')
     end
   end
 
   describe '#winningest_coach' do
     it "can return the coach with the best win loss percentage" do
-      searched_season = Season.new(20122013)
-      expect(searched_season.winningest_coach).to eq('John Hynes')
+      expect(@searched_season.winningest_coach).to eq('John Hynes')
     end
   end
 
   describe '#method_setup' do
     it 'can be used to setup the functionality of other methods' do
-      searched_season = Season.new(20122013)
-      expect(searched_season.method_setup).not_to eq(nil)
+      expect(@searched_season.method_setup).not_to eq(nil)
     end
   end
   
